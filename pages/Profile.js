20f123b29163d3c897ec07f6b4e52eb2e2742504
fatihis <isk.fatih1@gmail.com>
2020-12-08 21:22:53 +0300

    Update Profile page

--- pages/Profile.js
+++ pages/Profile.js
@@ -13,14 +13,38 @@ import {
   TextInput,
   FlatList,
 } from 'react-native';
+import ProfileData from './components/ProfileData';
 
 export default class Profile extends Component {
+  ListItem = ({item, index}) => {
+    return (
+      <TouchableOpacity
+        onPress={({item}) => this.itemOnPress}
+        style={styles.ItemComp}>
+        <Image style={styles.ItemImage} source={{uri: item.picture}}></Image>
+        <View style={styles.ItemTextWrapper}>
+          <Text>{item.name}</Text>
+          <Text>{item.star}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
   render() {
     return (
       <ImageBackground
         source={require('./assets/profilebg.jpg')}
         style={styles.bgimage}>
-        <FlatList></FlatList>
+        <FlatList
+          renderItem={this.ListItem}
+          keyExtractor={(item) => item._id}
+          data={ProfileData}
+          style={styles.FlatList}></FlatList>
+        <View style={styles.settingSection}>
+          <View style={styles.settingPart}></View>
+          <View style={styles.settingPart}></View>
+          <View style={styles.settingPart}></View>
+          <View style={styles.settingPart}></View>
+        </View>
       </ImageBackground>
     );
   }
@@ -31,5 +55,43 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     justifyContent: 'center',
     paddingHorizontal: 30,
+  },
+  settingPart: {
+    width: '100%',
+    height: 50,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: 'rgba(249, 199, 199, 0.6)',
+  },
+  ItemComp: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(235, 228, 228, 0.8)',
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+    flexDirection: 'row',
+    borderRadius: 30,
+  },
+  ItemImage: {
+    width: 120,
+    height: '100%',
+    marginHorizontal: 10,
+    borderRadius: 10,
+  },
+  ItemTextWrapper: {justifyContent: 'space-around'},
+  danisanlarText: {
+    alignSelf: 'center',
+    fontSize: 35,
+    marginTop: 20,
+  },
+  FlatList: {
+    flex: 1,
+    borderWidth: 3,
+    marginBottom: 10,
+  },
+
+  settingSection: {
+    flex: 3,
   },
 });
---
+++ pages/components/ProfileData.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    _id: '5c52de4c0f4ed2ae1bff8739',
+    picture: 'https://ui-avatars.com/api/?background=0D8ABC&color=fff',
+    name: 'Webster Burgess',
+    star: 'EQUITOX',
+  },
+];
